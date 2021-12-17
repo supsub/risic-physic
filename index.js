146353d9f3c87d58ff9ae762d644e949947ee0d0
julo <juliusz.rylko@slabs.pl>
2021-12-17 23:30:28 +0100

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+$(function() {
+    $.get('http://numbersapi.com/random', function(data) {
+      arr = data.split(' ') 
+      question = 'What '+ arr.slice(1, ).join(' ').slice(0, -1) + '?'
+      $('#question').text(question);
+      $('#number').text(arr[0]);
+    });
+  });
+  $(document).on('click', '#btn', function() {
+    $('#number').toggle() 
+  });
+
+  $(document).on('click', '#reloadbtn', function() {
+    window.location.reload(true);
+  });
